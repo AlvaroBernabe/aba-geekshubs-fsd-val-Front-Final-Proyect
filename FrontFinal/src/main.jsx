b9eccb35ae4482dce2a06fc366876c632f44b3a5
alvarito101093 <alvaro101093@gmail.com>
2023-04-18 14:14:54 +0200

    feat: main.jsx ready for redux

--- FrontFinal/src/main.jsx
+++ FrontFinal/src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import {Provider} from 'react-redux';
+import store from './app/store';
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-       <BrowserRouter>
-            <Router />
-        </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+              <Router />
+          </BrowserRouter>
+      </PersistGate>
+  </Provider>
+</React.StrictMode>,
 )
