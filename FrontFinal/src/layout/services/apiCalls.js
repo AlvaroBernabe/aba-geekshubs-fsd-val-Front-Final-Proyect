f7cc05abe9780d916b5cc376d5ff3ac13640dc4e
alvarito101093 <alvaro101093@gmail.com>
2023-04-18 16:43:10 +0200

    fix: Register Form working Correctly now and Connected to BBDD

--- FrontFinal/src/layout/services/apiCalls.js
+++ FrontFinal/src/layout/services/apiCalls.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 // import { useSelector } from 'react-redux';
 // import { userData } from '../userSlice';
 
-const root = "http://localhost:8000";
+
+const root = "http://localhost:8000/api";
 
 export const registerUser = async (body) => {
   return await axios.post(`${root}/register`, body)
