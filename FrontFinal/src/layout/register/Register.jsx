bf07d6071dc96769c2abb54a2c1295b9371f3161
alvarito101093 <alvaro101093@gmail.com>
2023-04-18 13:34:08 +0200

    feat: Register Form initial layout

--- FrontFinal/src/layout/register/Register.jsx
+++ FrontFinal/src/layout/register/Register.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import './Register.css'
+import "./Register.css";
 import NavBar from "../../components/NavBar";
+import { Button, Form } from "react-bootstrap";
 
 export const Register = () => {
-  return (
+    return (
     <>
-    < NavBar />
-  <div>Register</div>
-  <p>This is Register</p>
+    <NavBar />
+    <p>This is Register</p>
+    <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+    </Form>
     </>
-)};
+    );
+};
