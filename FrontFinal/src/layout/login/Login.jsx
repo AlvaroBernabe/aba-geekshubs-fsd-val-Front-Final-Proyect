e52c551671b83cbb1518a4925acd9b02af0bc7e7
alvarito101093 <alvaro101093@gmail.com>
2023-04-19 12:16:20 +0200

    feat: Login Form Now save Token and User Data

--- FrontFinal/src/layout/login/Login.jsx
+++ FrontFinal/src/layout/login/Login.jsx
@@ -1,12 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css'
 import NavBar from "../../components/NavBar";
+import { login } from "../userSlice";
+import { InputText } from "../../components/InputText/InputText";
+import { loginUser } from "../services/apiCalls";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import jwt_decode from "jwt-decode";
 
 export const Login = () => {
-    return (
-        <>
-        < NavBar />
-        <div>Login</div>
-        <p>This is Login</p>
-        </>
-      )};
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [welcome, setWelcome] = useState("");
+
+  const [credenciales, setCredenciales] = useState({
+  email: '',
+  password: ''
+  })
+
+  const inputHandler = (e) => {
+      setCredenciales((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+      }));
+}
+
+const checkError = (e) => {
+}
+
+const logMe = () => {
+  loginUser(credenciales)
+      .then( respuesta => {
+        const token = respuesta.data.token;
+        // console.log("esto es respuesta", respuesta);
+              let datosBackend = {
+                  token: respuesta.data.tokencoded,
+                  usuario: respuesta.data.token
+              };       
+            //   console.log("esto es datos backend",datosBackend)
+            let nameUser = datosBackend.usuario.abilities[1].toString();
+            console.log("esto es name ", nameUser)
+
+              
+              dispatch(login({credentials: datosBackend}));
+              setWelcome(`Bienvenid@ de nuevo ${nameUser}`);
+    //   setTimeout(() => {
+    //       navigate("/");
+    //     }, 3000);
+      })
+      .catch(error => console.log(error))
+};
+  return (
+      <>
+      <NavBar />
+      <hr />
+      <div className="divPrincipal">
+          {/* <div className="loginDesign">
+              {welcome !== "" ? (
+          <div>{welcome}</div>
+      ) : ( */}
+          <div>
+              <Container>
+                  <Row className="LoginForm">
+                      <Col lg={6}>
+                          <Form className="formLogin">
+                              <Form.Group>
+                                  <Form.Label>Enter your email account:</Form.Label>
+                                  <InputText className={"inputLogin"}
+                                  type={"email"} name={"email"} maxLength = {50} placeholder={"email..."} required={true}
+                                  changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
+                              </Form.Group>
+                              <Form.Group>
+                                  <Form.Label>Enter your password:</Form.Label>
+                                  <InputText className={"inputLogin"}
+                                  type={"password"} name={"password"} maxLength = {64} placeholder={""} required={true} 
+                                  changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
+                              </Form.Group>
+                              <br />
+                              <Button className="botonLog" variant="primary" 
+                              onClick={()=> logMe()}> Login User
+                              </Button>
+                          </Form>
+                      </Col>
+                  </Row>
+              </Container>
+          </div>
+          {/* )}
+          </div> */}
+      </div>
+      </>
+  );
+}
--- FrontFinal/src/layout/services/apiCalls.js
+++ FrontFinal/src/layout/services/apiCalls.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // import { useSelector } from 'react-redux';
 // import { userData } from '../userSlice';
+import api from './api';
 
 
 const root = "http://localhost:8000/api";
@@ -8,3 +9,8 @@ const root = "http://localhost:8000/api";
 export const registerUser = async (body) => {
   return await axios.post(`${root}/register`, body)
 }
+
+export const loginUser = async (body) => {
+  return await api.post(`${root}/login`, body)
+}
+
