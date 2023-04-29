42756c8578a7b4782f5e8a8204a0143b3da77f97
alvarito101093 <alvaro101093@gmail.com>
2023-04-29 20:31:44 +0200

    Style: Get My Reviews, Added Yes or No to Favourite from boolean

--- FrontFinal/src/components/CardMyReviews.jsx
+++ FrontFinal/src/components/CardMyReviews.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 function CardMyReviews({ appo }) {
     console.log(appo, "soy appo")
+    const isFavorite = (favorite) => {
+        return favorite ? "Yes" : "No";
+      };
     return (
         <Card style={{ width: "18rem" }}>
             <Card.Body>
@@ -10,7 +13,7 @@ function CardMyReviews({ appo }) {
                 <ul>
                     <li><span className="textColor">ID: </span>{appo.id}</li>
                     <li><span className="textColor">Game ID: </span>{appo.game_id}</li>
-                    <li><span className="textColor">Favorito: </span>{appo.favourite}</li>
+                    <li><span className="textColor">Favorite: </span> {isFavorite(appo.favourite)}</li>
                     <li><span className="textColor">Your Review: </span>{appo.player_review}</li>
                     <li><span className="textColor">Player Score: </span>{appo.player_score}</li>
                 </ul>
