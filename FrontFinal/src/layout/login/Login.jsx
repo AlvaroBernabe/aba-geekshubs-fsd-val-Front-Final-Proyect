d687f3fa2a2c3a00787a54dfcf363611e9544602
alvarito101093 <alvaro101093@gmail.com>
2023-04-29 11:37:58 +0200

    feat: Login Form ready again from JWT to Sanctum and Welcome error added

--- FrontFinal/src/layout/login/Login.jsx
+++ FrontFinal/src/layout/login/Login.jsx
@@ -4,17 +4,25 @@ import { InputText } from "../../components/InputText/InputText";
 import { loginUser } from "../services/apiCalls";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { Button, Col, Container, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, ListGroup, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 
 export const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [welcome, setWelcome] = useState("");
 
+  const renderTooltip = (props) => (
+    <Tooltip id="ButtonRegister-tooltip" {...props}>
+      You must enter a Valid User First
+    </Tooltip>
+  )
+
   const [credenciales, setCredenciales] = useState({
     email: "",
     password: "",
   });
+
+
 
   const inputHandler = (e) => {
     setCredenciales((prevState) => ({
@@ -22,6 +30,8 @@ export const Login = () => {
       [e.target.name]: e.target.value,
     }));
   };
+  const [registerAct, setRegisterAct] = useState(false);
+
 
   const checkError = (e) => { };
 
@@ -33,7 +43,7 @@ export const Login = () => {
           token: respuesta.data.token,
           usuario: respuesta.data.data,
         };
-          // console.log("esto es datos backend",datosBackend)
+        //   console.log("esto es datos backend",datosBackend)
         let nombre = datosBackend.usuario.email;
         dispatch(login({ credentials: datosBackend }));
         setWelcome(`Bienvenid@ de nuevo ${nombre}`);
@@ -41,64 +51,74 @@ export const Login = () => {
           navigate("/");
         }, 3000);
       })
-      .catch((error) => setWelcome(`Error email/password`, error));
-
+      .catch((error) => {
+        console.log(error);
+        setWelcome(`Email or Password incorrect`);
+        setTimeout(() => {
+          window.location.reload(true);
+        }, 2000);
+      })
   };
-  console.log(credenciales, "esto es user")
   return (
     <>
-      <div className="divPrincipal">
-        <div className="loginDesign">
-          {welcome !== "" ? (
-            <div>{welcome}</div>
-          ) : (
-            <div>
-              <Container>
-                <Row className="LoginForm">
-                  <Col lg={6}>
-                    <Form className="formLogin">
-                      <Form.Group>
-                        <Form.Label>Enter your email account:</Form.Label>
-                        <InputText
-                          className={"inputLogin"}
-                          type={"email"}
-                          name={"email"}
-                          maxLength={50}
-                          placeholder={"email..."}
-                          required={true}
-                          changeFunction={(e) => inputHandler(e)}
-                          blurFunction={(e) => checkError(e)}
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Enter your password:</Form.Label>
-                        <InputText
-                          className={"inputLogin"}
-                          type={"password"}
-                          name={"password"}
-                          maxLength={64}
-                          placeholder={""}
-                          required={true}
-                          changeFunction={(e) => inputHandler(e)}
-                          blurFunction={(e) => checkError(e)}
-                        />
-                      </Form.Group>
-                      <br />
-                      <Button
-                        className="botonLog"
-                        variant="primary"
-                        onClick={() => logMe()}
+      <div>
+        {welcome !== "" ? (
+          <div className="divWellcome">
+            <Card>
+              <Card.Header>{welcome}</Card.Header>
+            </Card>
+          </div>
+        ) : (
+          <div>
+            <Container>
+              <Row className="AllLoginForm">
+                <Col lg={6}>
+                  <Form className="FormLogin">
+                    <Form.Group>
+                      <Form.Label>Enter your email account:</Form.Label>
+                      <InputText
+                        className={"inputLogin"}
+                        type={"email"}
+                        name={"email"}
+                        maxLength={50}
+                        placeholder={"email..."}
+                        required={true}
+                        changeFunction={(e) => inputHandler(e)}
+                        blurFunction={(e) => checkError(e)}
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Enter your password:</Form.Label>
+                      <InputText
+                        className={"inputLogin"}
+                        type={"password"}
+                        name={"password"}
+                        maxLength={64}
+                        placeholder={"password..."}
+                        required={true}
+                        changeFunction={(e) => inputHandler(e)}
+                        blurFunction={(e) => checkError(e)}
+                      />
+                    </Form.Group>
+                    <br />
+                    <div className="ButtonLogin">
+                      <OverlayTrigger
+                        placement="right"
+                        delay={{ show: 100, hide: 300 }}
+                        overlay={renderTooltip}
                       >
-                        {" "}
-                        Login User
-                      </Button>
-                    </Form>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          )}
-        </div>
+                        <Button
+                          className="registerSendAct"
+                          onClick={() => logMe()}
+                        >
+                          Login User
+                        </Button></OverlayTrigger></div>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        )}
       </div>
     </>
   );
