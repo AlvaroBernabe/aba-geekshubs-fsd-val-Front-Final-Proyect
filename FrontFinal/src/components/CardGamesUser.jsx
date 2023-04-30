64d65e1092076f0c43bf9ff37ac143932da4971e
alvarito101093 <alvaro101093@gmail.com>
2023-04-30 22:20:03 +0200

    feat: Get All Games User => Added Find Search by Name, Genre,  Publisher, Score

---
+++ FrontFinal/src/components/CardGamesUser.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { isFavouriteReview } from "../layout/reviewSlice";
+import { useDispatch } from "react-redux";
+
+function CardGamesUser({ games }) {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function getStarScore(score) {
+    // console.log(score)
+    const maxScore = 10.00;
+    const fullStars = "⭐️".repeat(Math.floor(score));
+    const halfStar = score % 1 >= 0.5 ? "✮" : "";
+    const emptyStars = "✰".repeat(Math.floor(maxScore - score));
+    return `${fullStars}${halfStar}${emptyStars}`;
+  }
+
+  const scoreStar = getStarScore(games.score);
+
+  // console.log("esto es score", scoreStar);
+
+
+
+  const handleFavouriteYesClick = (games) => {
+    dispatch(isFavouriteReview({ choosenReview: games }))
+    console.log(games, "esto es review en teoria");
+    setTimeout(() => {
+      navigate('/review/new');
+    }, 500);
+  }
+
+  return (
+    <Card className="CardGames">
+      <Card.Img variant="top" src={games.game_image} />
+      <Card.Body>
+        <ul>
+          <span className="gameTitle">{games.name}</span>
+          <li>
+            <span className="textColor">Genre: </span>
+            {games.genre}
+          </li>
+          <li>
+            <span className="textColor">Score: </span>
+            {games.score} {scoreStar}
+          </li>
+          <li>
+            <span className="textColor">Description: </span>
+            {games.description}
+          </li>
+          <li>
+            <span className="textColor">Publisher: </span>
+            {games.publisher}
+          </li>
+          <li>
+            <span className="textColor">Release Date: </span>
+            {games.release_date}
+          </li>
+        </ul>
+        <div className="ButtonDivFavouriteGame">
+          <Button className="buttonFavorite" onClick={() => handleFavouriteYesClick(games)} key={games.id}>
+            Favourite?
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardGamesUser;
--- FrontFinal/src/layout/games/getAllGamesUser/GetAllGamesUser.jsx
+++ FrontFinal/src/layout/games/getAllGamesUser/GetAllGamesUser.jsx
@@ -3,17 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import { userData } from "../../userSlice";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { addChoosen } from "../../detailSlice";
 import { getAllGamesWithoutReviewUser } from "../../services/apiCalls";
-import { isFavouriteReview } from "../../reviewSlice";
+import CardGamesUser from "../../../components/CardGamesUser";
 
 export const GetAllGamesUser = () => {
 
   const [games, setGames] = useState([]);
   const ReduxCredentials = useSelector(userData);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+  //   const navigate = useNavigate();
+  const [searchName, setSearchName] = useState('');
+  const [searchGenre, setSearchGenre] = useState('');
+  const [searchPublisher, setSearchPublisher] = useState('');
+  const [searchScore, setSearchScore] = useState('');
 
   useEffect(() => {
     // console.log("console log de users", users)      // Este saca los el array con los usuarios
@@ -31,73 +35,34 @@ export const GetAllGamesUser = () => {
 
   const selected = (juego) => {
     dispatch(addChoosen({ choosenObject: juego }))
-    console.log(juego)
+    // console.log(juego)
     // setTimeout(() => {
     //     navigate("/users/all/details");
     //   }, 500);
   }
-
-  function getStarScore(score) {
-    // console.log(score)
-    const maxScore = 10.00;
-    const fullStars = "⭐️".repeat(Math.floor(score));
-    const halfStar = score % 1 >= 0.5 ? "✮" : "";
-    const emptyStars = "✰".repeat(Math.floor(maxScore - score));
-    return `${fullStars}${halfStar}${emptyStars}`;
-  }
-  
-  const scoreStar = getStarScore(games.score);
-  
-
-  const handleFavouriteYesClick = (games) => {
-    dispatch(isFavouriteReview({ choosenReview: games }))
-    console.log(games, "esto es review en teoria");
-    setTimeout(() => {
-      navigate('/review/new');
-    }, 500);
-  }
+console.log(games, "esto es games");
+  const findReviews = games.filter((game) => {
+    return game.name.toLowerCase().includes(searchName.toLowerCase()) &&
+        game.genre.toString().toLowerCase().includes(searchGenre.toLowerCase())&&
+        game.publisher.toString().toLowerCase().includes(searchPublisher.toLowerCase())&&
+        game.score >= searchScore;
+});
 
   return (
     <>
       <Container fluid>
-        <Row className="bodyGetFavourites">
-          {games.map((game) => {
+      <div className="search-bar">
+                    <input type="text" placeholder="Search By Name" onChange={(e) => setSearchName(e.target.value)} />
+                    <input type="text" placeholder="Search By Genre" onChange={(e) => setSearchGenre(e.target.value)} />
+                    <input type="text" placeholder="Search By Publisher" onChange={(e) => setSearchPublisher(e.target.value)} />
+                    <input type="number" placeholder=" Minimum Score" onChange={(e) => setSearchScore(e.target.value)} />
+                </div>
+        <Row  className="bodyGetFavourites">
+          {findReviews.map((game) => {
             console.log(game, "hola soy game");
             return (
               <Col onClick={() => selected(game)} key={game.id}>
-                <Card className="CardGames">
-                  <Card.Img variant="top" src={game.game_image} />
-                  <Card.Body>
-                    <ul>
-                      <span className="gameTitle">{game.name}</span>
-                      <li>
-                        <span className="textColor">Genre: </span>
-                        {game.genre}
-                      </li>
-                      <li>
-                        <span className="textColor">Score: </span>
-                        {game.score} {scoreStar}
-                      </li>
-                      <li>
-                        <span className="textColor">Description: </span>
-                        {game.description}
-                      </li>
-                      <li>
-                        <span className="textColor">Publisher: </span>
-                        {game.publisher}
-                      </li>
-                      <li>
-                        <span className="textColor">Release Date: </span>
-                        {game.release_date}
-                      </li>
-                    </ul>
-                    <div className="ButtonDivFavouriteGame">
-                      <Button className="buttonFavorite" onClick={() => handleFavouriteYesClick(game)} key={game.id}>
-                        Favourite?
-                      </Button>
-                    </div>
-                  </Card.Body>
-                </Card>
+                <CardGamesUser games={game} />
               </Col>
             );
           })}
